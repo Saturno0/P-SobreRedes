73917ee588ace2de6839b6abbf05662d24c8b99b
Lautar0 <104235333+Saturno0@users.noreply.github.com>
2025-04-27 20:07:44 -0300

    Update Main.java

--- Programacion Sobre Redes/src/Main.java
+++ Programacion Sobre Redes/src/Main.java
@@ -1,14 +1,37 @@
 //import Tienda.Tienda;
-//import Utilidades.utilidades;
-import Curso.Tienda;
+import SimCity.Ciudad;
+import SimCity.Edificio;
+import SimCity.Jugador;
+import SimCity.Tienda;
+import Utilidades.utilidades;
+//import Curso.Tienda;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println("Vienvenido al juego del SimCity");
+		System.out.println("Ingrese el nombre de su ciudad: ");
+		String nombreCiudad = utilidades.s.nextLine();
+		
+		Ciudad ciudad = new Ciudad(nombreCiudad, crearJugador());
+		
 		Tienda tienda = new Tienda();
-		tienda.comprar();
+		
+		Edificio[] edificios = tienda.comprarEdificios(ciudad.getAlcalde());
+		ciudad.setEdificios(edificios);
+		
+		ciudad.menu();
+		
+		utilidades.s.close();
+	}
+
+	
+
+	private static Jugador crearJugador() {
+		System.out.println("Ingrese su nombre como alcalde");
+		String nombre = utilidades.s.nextLine();
+		return new Jugador(nombre);
 	}
 
 //	private static String ingresarNombre() {
